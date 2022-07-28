2b395e9f2309c8b8145c3ef16ea0682fac9f9561
Your Name <dat054760@gmail.com>
2022-07-29 03:16:12 +0700

    up

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,13 @@
+package rikkei.academy;
+
+public class Main {
+    public static void main(String[] args) {
+        Student.change();
+        Student s1 = new Student(111,"chinh");
+        Student s2 = new Student(222,"Toan");
+        Student s3 = new Student(333,"Dung");
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
---
+++ src/rikkei/academy/Student.java
@@ -0,0 +1,17 @@
+package rikkei.academy;
+
+public class Student {
+    private int rollNo;
+    private String name;
+    private static String college = "BBDIT";
+    Student(int r,String n){
+        rollNo = r;
+        name = n;
+    }
+    static void change(){
+        college = "RIKKEI ACADEMY";
+    }
+    void display(){
+        System.out.println(rollNo+""+name+""+college);
+    }
+}
